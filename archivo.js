bd217b38926e9aa33fac121d835ff816309a03e2
antotemu <antonellatemudio@gmail.com>
2023-09-12 10:20:39 -0300

    primer commit

---
+++ archivo.js
@@ -0,0 +1,11 @@
+const h2 = document.querySelector('h2');
+
+h2.addEventListener('click', () => {
+    alert('Hiciste clic en el encabezado del mapa.');
+    // Agrega aquí la lógica adicional que desees ejecutar al hacer clic en el encabezado.
+});
+
+
+
+
+
