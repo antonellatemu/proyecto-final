c50dd717c4bee86788bd529848683b47da139be2
antotemu <antonellatemudio@gmail.com>
2023-09-26 20:07:18 -0300

    sexto commit

--- archivo.js
+++ archivo.js
@@ -20,14 +20,42 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('La animación ha terminado.');
     });
 });
-const hr = document.querySelector('hr');
 
-hr.addEventListener('click', () => {
-  hr.style.transform = 'scaleX(2)';
-  setTimeout(() => {
-    hr.style.transform = 'scaleX(1)';
-  }, 1000); // Revertir después de 1 segundo
-});
+const textos = ["Centros de alta complejidad municipales y privados",]; // Lista de textos a mostrar y borrar
+const h2Element = document.getElementById("centros");
+let indiceTexto = 0; // Índice del texto actual
+
+function escribirTexto() {
+    const textoActual = textos[indiceTexto];
+    let textoVisible = "";
+    let intervalo = setInterval(function () {
+        textoVisible += textoActual.charAt(textoVisible.length);
+        h2Element.textContent = textoVisible;
+
+        if (textoVisible === textoActual) {
+            clearInterval(intervalo);
+            setTimeout(borrarTexto, 1000); // Borra el texto después de 1 segundo
+        }
+    }, 100); // Velocidad de escritura (en milisegundos)
+}
+
+function borrarTexto() {
+    let textoActual = h2Element.textContent;
+    let intervalo = setInterval(function () {
+        textoActual = textoActual.slice(0, -1);
+        h2Element.textContent = textoActual;
+
+        if (textoActual === "") {
+            clearInterval(intervalo);
+            indiceTexto = (indiceTexto + 1) % textos.length; // Avanzar al siguiente texto
+            setTimeout(escribirTexto, 1000); // Escribe el próximo texto después de 1 segundo
+        }
+    }, 100); // Velocidad de borrado (en milisegundos)
+}
+
+escribirTexto(); // Comienza el proceso de escritura
+
+
 
 
 
